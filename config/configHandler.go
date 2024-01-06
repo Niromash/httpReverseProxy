87e66e4f47398e459111221416ef5054d399db7f
Mattéo <bl4ise.chr@gmail.com>
2024-01-06 22:14:56 +0100

    Update

--- config/configHandler.go
+++ config/configHandler.go
@@ -27,16 +27,16 @@ redirectTo = "http://api_app:3000"
 `
 
 func HandleConfigFile() ([]byte, error) {
-	file, err := os.ReadFile("./config.toml")
+	file, err := os.ReadFile("/etc/httpReverseProxy/config.toml")
 	if err != nil {
 		if os.IsNotExist(err) {
-			log.Println("./config.toml file not found, trying to create it...")
+			log.Println("/etc/httpReverseProxy/config.toml file not found, trying to create it...")
 			file = []byte(configSample)
-			if err := os.WriteFile("./config.toml", file, 777); err != nil {
+			if err := os.WriteFile("/etc/httpReverseProxy/config.toml", file, 777); err != nil {
 				log.Fatal("Unable to create default config file: " + err.Error())
 				return nil, err
 			}
-			log.Println("./config.toml file created with sample config.")
+			log.Println("/etc/httpReverseProxy/config.toml file created with sample config.")
 			return file, nil
 		}
 
--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 )
 
 func main() {
-
 	sigCh := make(chan os.Signal, 1)
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
@@ -31,35 +30,6 @@ func main() {
 		log.Println("The application is stopping, please wait few seconds")
 		os.Exit(0)
 	}()
-
-	//atoi, err := strconv.Atoi(os.Getenv("PORT"))
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
-	//cfg := api.Config{
-	//	HttpServerOptions: api.HttpServerOptions{
-	//		Port: atoi,
-	//	},
-	//	Preferences: api.Preferences{
-	//		HostnameNotFoundMessage: "Not found",
-	//	},
-	//	ProxyHost: []api.ProxyHost{
-	//		{
-	//			Hostname:   "localhost",
-	//			RedirectTo: "https://www.twitch.tv",
-	//			PathType:   api.PathTypePrefix,
-	//			Path:       "/directory",
-	//		},
-	//		{
-	//			Hostname:   "localhost",
-	//			RedirectTo: "https://blog.filador.fr",
-	//			//RedirectTo: "https://emeralds-hub.com",
-	//			PathType: api.PathTypePrefix,
-	//			Path:     "/",
-	//		},
-	//	},
-	//}
 
 	cfgRaw, err := config.HandleConfigFile()
 	if err != nil {
@@ -95,30 +65,20 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
+
 			proxy := httputil.NewSingleHostReverseProxy(remote)
-
-			proxy.Director = func(req *http.Request) {
-				// Remove trailing slash
-				//wildcardIndex := strings.IndexAny("/*catchall", "*")
-				//proxyPath := singleJoiningSlash(remote.Path, r.URL.Path[wildcardIndex:])
-				//if strings.HasSuffix(proxyPath, "/") && len(proxyPath) > 1 {
-				//	proxyPath = proxyPath[:len(proxyPath)-1]
-				//}
-				//fmt.Println(proxyPath)
-
-				req.URL.Scheme = remote.Scheme
-				req.URL.Host = remote.Host
-				req.Host = remote.Host
-			}
 
 			log.Printf("Redirecting %s to %s", hostname, remote.Host)
 			r.Header.Set("Cache-control", "no-cache")
+
+			// Todo can possibly cause issue with some websites, because the host is not the url entered by user, but the one of the proxy
+			r.Header.Set("X-Forwarded-For", r.RemoteAddr)
 			r.Header.Set("Host", utils.IfThenElse(r.TLS == nil, "http://", "https://")+remote.Host)
 			r.Header.Set("Referer", r.Referer())
 			r.Header.Set("X-Forwarded-Host", proxyHost.Hostname)
 			r.Header.Set("X-Forwarded-Proto", r.Proto)
 			r.Header.Set("X-Forwarded-Port", strconv.Itoa(cfg.HttpServerOptions.Port))
-			r.Header.Add("X-Origin-Host", remote.Host)
+			r.Header.Set("X-Origin-Host", remote.Host)
 
 			if _, err = net.LookupIP(remote.Hostname()); err != nil {
 				log.Println("Target cannot be reached:", err.Error())
@@ -134,7 +94,7 @@ func main() {
 		} else {
 			w.WriteHeader(404)
 			if cfg.Preferences.UseIndexFileWhenHostnameNotFound {
-				indexFile, err := os.OpenFile("./"+cfg.Preferences.IndexFilePath, os.O_RDONLY, 777)
+				indexFile, err := os.OpenFile("/etc/httpReverseProxy/"+cfg.Preferences.IndexFilePath, os.O_RDONLY, 777)
 				if err == nil {
 					indexFileContent, err := ioutil.ReadAll(indexFile)
 					if err == nil {
@@ -157,16 +117,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func singleJoiningSlash(a, b string) string {
-	aslash := strings.HasSuffix(a, "/")
-	bslash := strings.HasPrefix(b, "/")
-	switch {
-	case aslash && bslash:
-		return a + b[1:]
-	case !aslash && !bslash:
-		return a + "/" + b
-	}
-	return a + b
 }
